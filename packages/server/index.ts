3d68a89cfacac1aa93afa5e18999e0822cb60999
vesh <vesh8750@gmail.com>
2025-09-29 09:20:55 +0530

    created server and frontend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,53 @@
 import express from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai";
+import morgan from "morgan";
 import type { Request, Response } from "express";
+import z from "zod";
 
 dotenv.config();
 
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello bun");
+});
+
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+  prompt: z.string().trim().min(5).max(500),
+  conversationId: z.string(),
+});
+
+app.post("/api/chat", async (req: Request, res: Response) => {
+  const parseResult = chatSchema.safeParse(req.body);
+
+  if (!parseResult.success) {
+    const formattedError = z.treeifyError(parseResult.error);
+    return res.status(400).json({ error: formattedError });
+  }
+
+  const { prompt, conversationalId } = req.body;
+
+  const response = await client.responses.create({
+    model: "gpt-4o-mini",
+    input: prompt,
+    temperature: 0.2,
+    max_output_tokens: 100,
+    previous_response_id: conversations.get(conversationalId) || undefined,
+  });
+
+  conversations.set(conversationalId, String(response.id));
+
+  res.json({ message: response.output_text });
 });
 
 const PORT = process.env.PORT || 3000;
